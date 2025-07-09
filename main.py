2704a0f807cdad0967f449d1195c74ac4085bf99
maksym <1maksymrusanov1@gmail.com>
2025-07-09 19:15:44 +0100

    add about page

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app=Flask(__name__)
 @app.route('/')
 def main_page():
     return '<h1>main page<h1>'
+@app.route('/about')
+def about():
+    return '<h1>about us<h1>'
 
 if __name__=='__main__':
     app.run(debug=True)
