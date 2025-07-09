04f60dd0e9869e47e9fcd06963a9d3579bc59fde
maksym <1maksymrusanov1@gmail.com>
2025-07-09 21:40:47 +0100

    add some staff

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, request, redirect
+
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    text = db.Column(db.Text)
 
 
 @app.route("/")
-def main_page():
-    return render_template("base.html")
+def main_page(methods=["GET"]):
+    if request.method == "GET":
+        notes = Note.query.all()
+
+    return render_template("main.html", notes=notes)
+
+
+@app.route("/create", methods=["GET", "POST"])
+def create_new_task():
+    if request.method == "POST":
+        title = request.form["title"]
+        text = request.form["text"]
+        new_note = Note(title=title, text=text)
+        db.session.add(new_note)
+        db.session.commit()
+        return render_template("create_task.html", message="Note saved successfully!")
+
+    return render_template("create_task.html")
 
 
 if __name__ == "__main__":
