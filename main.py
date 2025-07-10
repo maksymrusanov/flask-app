c7a79e13ae0831c288bac08efe1d8438d9080bba
maksym <1maksymrusanov1@gmail.com>
2025-07-10 21:23:05 +0100

    finished flask app

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -14,13 +14,15 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     text = db.Column(db.Text)
+    status = db.Column(db.Boolean)
 
 
 @app.route("/")
 def main_page(methods=["GET"]):
     if request.method == "GET":
-        notes = Note.query.all()
-
+        if not Note.query.all():
+            return render_template("main.html")
+    notes = Note.query.all()
     return render_template("main.html", notes=notes)
 
 
@@ -29,7 +31,8 @@ def create_new_task():
     if request.method == "POST":
         title = request.form["title"]
         text = request.form["text"]
-        new_note = Note(title=title, text=text)
+        status = False
+        new_note = Note(title=title, text=text, status=status)
         db.session.add(new_note)
         db.session.commit()
         return render_template("create_task.html", message="Note saved successfully!")
@@ -37,6 +40,35 @@ def create_new_task():
     return render_template("create_task.html")
 
 
+@app.route("/delete/<int:note_id>", methods=["POST", "GET"])
+def delete_task(note_id):
+    note = Note.query.get(note_id)
+    if note:
+        db.session.delete(note)
+        db.session.commit()
+    return redirect(url_for("main_page"))
+
+
+@app.route("/mark_done/<int:note_id>", methods=["POST", "GET"])
+def mark_done(note_id):
+    note = Note.query.get(note_id)
+    if not note:
+        return redirect(url_for("main_page"))
+    note.status = True
+    db.session.commit()
+    return redirect(url_for("marked_done"))
+
+
+@app.route("/marked_done")
+def marked_done():
+    notes = Note.query.filter_by(status=True).all()
+    return render_template("marked_done.html", notes=notes)
+
+
+@app.route("/not_finished")
+def not_finished():
+    notes = Note.query.filter_by(status=False).all()
+    return render_template("not_finished.html", notes=notes)
 
 
 if __name__ == "__main__":
